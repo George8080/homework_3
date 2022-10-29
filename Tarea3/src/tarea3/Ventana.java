e1bdb89c5a551e82dd4910a639560df9082bea7a
daninonino <danielanovoao600@gmail.com>
2022-10-29 20:53:31 -0300

    cambio de la localización

--- Tarea3/src/tarea3/Ventana.java
+++ Tarea3/src/tarea3/Ventana.java
@@ -7,8 +7,7 @@ public class Ventana extends JFrame{
     public Ventana(){
         this.setSize(500, 500);     //Establecemos el tamaño de la ventana
         setTitle("Ventana");
-        setLocation(750, 200);
-        
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);    //Cierra la ventana
         
     }
